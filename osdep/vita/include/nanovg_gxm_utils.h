3a199a10df582d21ad4cf7b8350a8f67cb0ebb39
xfangfang <2553041586@qq.com>
2025-01-14 15:50:30 +0800

    Update nanovg

--- osdep/vita/include/nanovg_gxm_utils.h
+++ osdep/vita/include/nanovg_gxm_utils.h
@@ -47,14 +47,15 @@ extern "C"
 #define DISPLAY_COLOR_SURFACE_TYPE SCE_GXM_COLOR_SURFACE_LINEAR
 #define DISPLAY_PIXEL_FORMAT SCE_DISPLAY_PIXELFORMAT_A8B8G8R8
 
-static int gxm__error_status = SCE_OK;
 #define GXM_PRINT_ERROR(status) sceClibPrintf("[line %d] failed with reason: %s\n", __LINE__, gxmnvg__easy_strerror(status))
-#define GXM_CHECK_RETURN(func, ret)         \
-    gxm__error_status = func;               \
-    if (gxm__error_status != SCE_OK)        \
-    {                                       \
-        GXM_PRINT_ERROR(gxm__error_status); \
-        return ret;                         \
+#define GXM_CHECK_RETURN(func, ret)  \
+    {                                \
+        int status = func;           \
+        if (status != SCE_OK)        \
+        {                            \
+            GXM_PRINT_ERROR(status); \
+            return ret;              \
+        }                            \
     }
 #define GXM_CHECK(func) GXM_CHECK_RETURN(func, 0)
 #define GXM_CHECK_VOID(func) GXM_CHECK_RETURN(func, )
@@ -169,7 +170,7 @@ int gxmCreateVertexProgram(SceGxmShaderPatcherId programId,
 
 NVGXMwindow *gxmCreateWindow(const NVGXMinitOptions *opts);
 
-NVGXMwindow *gxmGetWindow();
+NVGXMwindow *gxmGetWindow(void);
 
 void gxmDeleteWindow(NVGXMwindow *window);
 
@@ -638,9 +639,9 @@ NVGXMwindow *gxmCreateWindow(const NVGXMinitOptions *opts) {
                                          "	return float4(position, 1.f, 1.f);\n"
                                          "}\n";
 
-    static const char *clearFragShader = "float4 main(uniform float4 color) : COLOR\n"
+    static const char *clearFragShader = "__nativecolor __regformat unsigned char4 main(uniform float4 color) : COLOR\n"
                                          "{\n"
-                                         "	return color;\n"
+                                         "	return unsigned char4(color * 255);\n"
                                          "}\n";
 #else
     static const unsigned char clearVertShader[252] = {
@@ -769,7 +770,7 @@ void gxmDeleteWindow(NVGXMwindow *window) {
     free(window);
 }
 
-NVGXMwindow *gxmGetWindow() {
+NVGXMwindow *gxmGetWindow(void) {
     return gxm_internal.window;
 }
 
@@ -1093,7 +1094,7 @@ void dumpShader(const char *name, const char *type, const SceGxmProgram *program
     FILE *fp = fopen(path, "w");
     if (fp) {
         fprintf(fp, "static const unsigned char %s%sShader[%i] = {", name, type, size);
-        for (int i = 0; i < size; ++i) {
+        for (uint32_t i = 0; i < size; ++i) {
             if (need_comma)
                 fprintf(fp, ", ");
             else
